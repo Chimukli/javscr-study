2529dadef4f02de603889a1f7415a20c93b477b5
Chimukli <bhagyashriw38@gmail.com>
2024-06-14 03:48:13 +0000

    string interpolation

---
+++ Birthday/string.js
@@ -0,0 +1,7 @@
+const name = "Sonal"
+const repocount = 50
+// console.log (name + repocount + "value");
+
+console.log(`Hello my name is ${name} and my repocount is ${repocount}`);
+console.log(name.charAt(2));
+console.log(name.indexOf('S'));
